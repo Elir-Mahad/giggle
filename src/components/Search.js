29e0ddf68d5ae495f379e290d95119842e3f3ce8
elir-mahad <mmahad280@gmail.com>
2020-10-19 16:30:59 -0400

    added the search component

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Search.css";
+
+function Search() {
+	return (
+		<div className="search">
+			<div className="search_input"></div>
+		</div>
+	);
+}
+
+export default Search;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import "../pages/Home.css";
 import { Link } from "react-router-dom";
 import AppsIcon from "@material-ui/icons/Apps";
 import { Avatar } from "@material-ui/core";
+import Search from "../components/Search";
 
 function Home() {
 	return (
@@ -29,6 +30,9 @@ function Home() {
 					src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
 					alt=""
 				/>
+				<div className="home_inputContainer">
+					<Search />
+				</div>
 			</div>
 		</div>
 	);
