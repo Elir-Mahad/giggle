bd5da58e86561f454881158bf916dafda103b01a
elir-mahad <mmahad280@gmail.com>
2020-10-19 16:34:34 -0400

    added the input field and search icon in the search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Search.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Search() {
 	return (
 		<div className="search">
-			<div className="search_input"></div>
+			<div className="search_input">
+				<SearchIcon className="search_inputIcon" />
+				<input />
+			</div>
 		</div>
 	);
 }
