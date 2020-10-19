121e6dab2bf81e286d1eca5707e5a899b0ecad55
elir-mahad <mmahad280@gmail.com>
2020-10-19 17:32:33 -0400

    adding usestate and functionality to buttons

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
 
 function Search() {
+	//
+	const [input, setInput] = useState("");
+	//
+	const search = e => {
+		e.preventDefault();
+	};
 	return (
 		<div className="search">
+			{/*  */}
 			<div className="search_input">
+				{/*  */}
 				<SearchIcon className="search_inputIcon" />
-				<input />
+				{/*  */}
+				<input
+					//
+					value={input}
+					onChange={e => setInput(e.target.valye)}
+				/>
 				<MicIcon />
+				{/*  */}
 			</div>
+
 			<div className="search_buttons">
-				<Button variant="outlined">Google Search</Button>
+				{/*  */}
+				<Button
+					//
+					onClick={search}
+					variant="outlined"
+				>
+					Google Search
+				</Button>
+
 				<Button variant="outlined">I'm feeling lucky</Button>
+				{/*  */}
 			</div>
 		</div>
 	);
