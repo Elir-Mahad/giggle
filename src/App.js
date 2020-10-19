e582c0fe21ab88f1c98e9d2df3d6bd82c569a335
elir-mahad <mmahad280@gmail.com>
2020-10-19 10:33:10 -0400

    added components and pages folder, added the home component inside the pages folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Giggle</h1>
 
+			<Home />
 			{/* Home page - contains search bar */}
 			{/* Results page - contains search results */}
 		</div>
