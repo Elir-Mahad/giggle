d469748940cd79cdac5768361328ad1c67d78f94
elir-mahad <mmahad280@gmail.com>
2020-10-19 10:58:18 -0400

    added the code to make react router on the app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Giggle</h1>
-
-			<Home />
-			{/* Home page - contains search bar */}
-			{/* Results page - contains search results */}
+			<Router>
+				<Switch>
+					<Route path="/search">
+						<h1>Giggle landing page</h1>
+						{/* Results page - contains search results */}
+					</Route>
+					<Route path="/">
+						{/* Home page - contains search bar */}
+						<Home />
+					</Route>
+				</Switch>
+			</Router>
 		</div>
 	);
 }
