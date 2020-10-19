9215ac121d68148ef019e5c379dbadfdd76e0172
elir-mahad <mmahad280@gmail.com>
2020-10-19 11:06:07 -0400

    added the links on the home component using react router dom

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "../pages/Home.css";
+import { Link } from "react-router-dom";
 
 function Home() {
 	return (
-		<div>
-			<h1>Home</h1>
+		<div className="home">
+			<h1>Home page</h1>
 
 			<div className="home_header">
 				{/*  */}
 				<div className="home_headerLeft">
-					{/* Link */}
-					{/* Link */}
+					<Link to="/about">About</Link>
+					<Link to="/store">Store</Link>
 				</div>
 
 				<div className="home_headerRight">
-					{/* Link */}
-					{/* Link */}
+					<Link to="/gmail">Gmail</Link>
+					<Link to="/images">Images</Link>
 					{/* Icon */}
 					{/* Avatar */}
 				</div>
