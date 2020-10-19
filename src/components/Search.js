fee58e1cc314b1af133bf4582b54265dc8f1aaf4
elir-mahad <mmahad280@gmail.com>
2020-10-19 17:06:48 -0400

    added buttons from materialui  under search bar and added the styles for button

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
+import { Button } from "@material-ui/core";
 
 function Search() {
 	return (
@@ -10,6 +11,10 @@ function Search() {
 				<SearchIcon className="search_inputIcon" />
 				<input />
 				<MicIcon />
+			</div>
+			<div className="search_buttons">
+				<Button variant="outlined">Google Search</Button>
+				<Button variant="outlined">I'm feeling lucky</Button>
 			</div>
 		</div>
 	);
