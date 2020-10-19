d10ee30994f150fd72c73f73cf7fdc3c2a5f2329
elir-mahad <mmahad280@gmail.com>
2020-10-19 10:41:01 -0400

    adding the different divs in the home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,24 @@ function Home() {
 	return (
 		<div>
 			<h1>Home</h1>
+
+			<div className="home_header">
+				{/*  */}
+				<div className="home_headerLeft">
+					{/* Link */}
+					{/* Link */}
+				</div>
+
+				<div className="home_headerRight">
+					{/* Link */}
+					{/* Link */}
+					{/* Icon */}
+					{/* Avatar */}
+				</div>
+				{/*  */}
+			</div>
+
+			<div className="home_body"></div>
 		</div>
 	);
 }
