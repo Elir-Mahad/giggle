65342307625c04d7d436229a31c1f9b9280c2fcf
elir-mahad <mmahad280@gmail.com>
2020-10-26 19:07:25 -0400

    added the google api keys

---
+++ src/Keys.js
@@ -0,0 +1,7 @@
+// We are using the api from google
+
+// https://developers.google.com/custom-search/v1/using_rest
+
+export const API_KEY = "AIzaSyBGOSAFwBB6u-_X1h-Mo4PzUsVuGmVBWfk";
+
+export default API_KEY;
