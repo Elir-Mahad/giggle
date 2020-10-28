2499ec9ce27744d660150835ceed318d3a044258
elir-mahad <mmahad280@gmail.com>
2020-10-28 15:35:15 -0400

    Updates

---
+++ precache-manifest.c8a6d7d29cacf59c7539843e31e0ae3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720cf7d2aaa1257cf480785bb02cbc6f",
+    "url": "/giggle/index.html"
+  },
+  {
+    "revision": "aa6d6948f2454307d1fd",
+    "url": "/giggle/static/css/main.82135d77.chunk.css"
+  },
+  {
+    "revision": "ca1107e0f0fc94513e1b",
+    "url": "/giggle/static/js/2.e4331266.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/giggle/static/js/2.e4331266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa6d6948f2454307d1fd",
+    "url": "/giggle/static/js/main.4063b8c1.chunk.js"
+  },
+  {
+    "revision": "20b7c01b6238f99a3413c7c13a1dab42",
+    "url": "/giggle/static/js/main.4063b8c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0541b321077d555528",
+    "url": "/giggle/static/js/runtime-main.bdce2bb9.js"
+  }
+]);
